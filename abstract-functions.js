30900b4a3003c427325b95516a730b724afda86d
Christian Hillebrand <ch.hillebrand@googlemail.com>
2021-03-09 16:38:26 +0100

    file altered

--- abstract-functions.js
+++ abstract-functions.js
@@ -1,34 +1,10 @@
 // Clean up the code below with a single function called `personalIntroduction`
 // The new code should have the exact same functionality and output.
+const introduction = (name, height, favoriteColor) => {
+    return `My name is ${name} and my height is ${height} and my favorite color is ${favoriteColor}`;
+};
 
-var name = "Bob";
-var height = "72";
-var favoriteColor = "red";
-var string = "My name is " + name;
-string += ", and my height is " + height;
-string += ", and my favorite color is " + favoriteColor;
-console.log(string);
-
-var name = "Samantha";
-var height = "65";
-var favoriteColor = "orange";
-var string = "My name is " + name;
-string += ", and my height is " + height;
-string += ", and my favorite color is " + favoriteColor;
-console.log(string);
-
-var name = "Christopher";
-var height = "69";
-var favoriteColor = "green";
-var string = "My name is " + name;
-string += ", and my height is " + height;
-string += ", and my favorite color is " + favoriteColor;
-console.log(string);
-
-var name = "Tamera";
-var height = "63";
-var favoriteColor = "blue";
-var string = "My name is " + name;
-string += ", and my height is " + height;
-string += ", and my favorite color is " + favoriteColor;
-console.log(string);
+console.log(introduction("Bob", "72", "red"));
+console.log(introduction("Samantha", "65", "orange"));
+console.log(introduction("Christopher", "69", "green"));
+console.log(introduction("Tamera", "63", "blue"));
